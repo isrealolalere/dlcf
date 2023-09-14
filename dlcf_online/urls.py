0fc7545f529999d23f76e1d3d326c5e4fbe03776
isrealolalere <isrealolalere09@gmail.com>
2023-09-14 21:46:36 +0100

    first commit

---
+++ dlcf_online/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from dlcf_online import views
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('freshers', views.all_freshers, name='freshers'),
+    path('update/<int:id>', views.update, name='update-fresher'),
+    path('fresher-info/<int:id>', views.fresher_info, name='fresher-info'),
+    path('search', views.search, name='search'),
+]
